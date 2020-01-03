3880cc8008b012ee80604b85b8525ca6a0f5af19
caojian <647812411@qq.com>
2020-01-03 18:01:31 +0800

    add files

---
+++ src/Swiftpass/Serivice/Entry.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Xcrms\Swiftpass\Serivice;
+
+
+use Xcrms\Swiftpass\Swiftpass;
+
+class Entry extends Swiftpass
+{
+
+}
---
+++ src/Swiftpass/Serivice/Pay.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Xcrms\Swiftpass\Serivice;
+
+
+use Xcrms\Swiftpass\Swiftpass;
+
+class Pay extends Swiftpass
+{
+
+}
